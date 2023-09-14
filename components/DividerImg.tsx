1d16ea5bcfdb61dcd4b8d2f6b781a35fe9c0e1af
Mullajonov Muhsinjon <122345056+al-muhsinjon@users.noreply.github.com>
2023-09-14 22:44:57 +0500

    udated

---
+++ components/DividerImg.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DividerImg = () => {
+  return (
+    <div>DividerImg</div>
+  )
+}
+
+export default DividerImg
---
+++ components/TeamWork.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TeamWork = () => {
+  return (
+    <div>TeamWork</div>
+  )
+}
+
+export default TeamWork
