f14f105179400a7346f55dc73c8a53a189e13c1d
Mullajonov Muhsinjon <122345056+al-muhsinjon@users.noreply.github.com>
2023-09-14 23:07:42 +0500

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,135 @@
-import Image from 'next/image'
+"use client";
+import {
+  AboutItem,
+  Button,
+  Culture,
+  DividerImg,
+  Hero,
+  Sponsor,
+  TeamWork,
+  Testimonials,
+  Work,
+} from "@/components";
+import { aboutData, information, storyData } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Hero />
+      <div className="w-full h-[90vh] items-center flex justify-center  px-11">
+        <div className="w-[65%] h-[62%] grid grid-cols-3 grid-rows-1  gap-y-24">
+          <div className="text-start m-auto flex flex-col gap-y-6 ">
+            <p className="text-lg">WHAT WE DO</p>
+            <h2 className="font-semibold text-5xl">EXPERTS</h2>
+            <p className="text-base italic">We build digital solutions.</p>
+            <DividerImg width={70} height={10} className="object-conatin" />
+          </div>
+          {aboutData.map((data) => (
+            <div
+              className="text-center cursor-pointer bg-slate-100 hover:shadow-lg flex rounded-md hover:bg-white group transition-all ease-in-out duration-75 flex-col justify-around py-4 gap-6 m-auto"
+              key={data.id}
+            >
+              <AboutItem data={data} />
+              <button className="uppercase">learn more</button>
+            </div>
+          ))}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="w-full h-[50vh] bg-design bg-fixed z-auto flex justify-center gap-y-8 items-center flex-col relative text-white">
+        <div className="w-full h-full absolute z-10 bg-[#0000006c]" />
+        <p className="z-50 relative font-josefin text-3xl font-light">
+          BEST EVER DESIGN
+        </p>
+        <h2 className="relative z-40 font-josefin text-5xl font-bold ">
+          Time to enhance your web presence!
+        </h2>
+        <Button
+          handleClick={() => {}}
+          className="relative z-40"
+          title="get quotation"
         />
       </div>
+      <Work />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="w-full h-[70vh] flex justify-center items-center ">
+        <div className="w-[60%] h-full">
+          <div className="text-start  flex flex-col gap-y-6">
+            <p className="text-3xl font-light">THE LIFE SIZE</p>
+            <h2 className="text-5xl font-bold">BRAND STORY</h2>
+            <p className="text-xl italic">
+              View multiple content block layouts under features section.
+            </p>
+            <DividerImg width={70} height={10} className="object-contain" />
+          </div>
+          <div className="flex mt-6 justify-evenly gap-24">
+            <div className="w-[50%]">
+              <p className="text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magnaal qua. Ut
+                enim ad minim veniam, quis nostrud exercitation ulla mco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+              <p className="text-base mt-6 leading-relaxed">
+                Excepteur sint occaecat cupidatat non proident, sunt in culp a
+                qui officia deserunt mollit anim id est laborum. Sed ut per
+                spiciatis unde omnis iste natus error sit voluptatem accusan
+                tium doloremque laudantium, totam rem aperiam, eaque ip
+              </p>
+            </div>
+            <div className="w-[50%]">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <p className="grid mt-6 gap-y-4 grid-cols-2">
+                {storyData.map((story) => (
+                  <Link
+                    className="text-xs text-btn-color"
+                    href={story.href}
+                    key={story.id}
+                  >
+                    {story.name}
+                  </Link>
+                ))}
+              </p>
+            </div>
+          </div>
+          <Button
+            className="mt-20"
+            handleClick={() => {}}
+            title="view proects"
+            blacked
+          />
+        </div>
       </div>
-    </main>
-  )
+      <Culture />
+      <TeamWork />
+
+      <div className="bg-visual bg-cover mt-24 bg-fixed w-full flex h-96 bg-no-repeat ">
+        <div className="w-full h-full flex bg-[#00000095]">
+          {information.map((info) => (
+            <div
+              key={info.id}
+              className="w-full h-full hover:bg-btn-color gap-y-3 flex flex-col justify-center items-center "
+            >
+              <span className="text-white">
+                <info.icon size={50} />
+              </span>
+              <h2 className="text-[#c1c5cd] text-3xl font-semibold">
+                {info.numeric}
+              </h2>
+              <h2 className="text-white uppercase text-2xl">{info.title}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <Testimonials />
+
+      <Sponsor />
+    </>
+  );
 }
